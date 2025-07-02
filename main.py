c16df096f48608a6ea041041d4ecd335e3426be4
ldc-369 <ldc369tpqt@gmail.com>
2025-07-03 02:22:31 +0700

    commit

--- main.py
+++ main.py
@@ -14,26 +14,33 @@ with open('./model/Y_scaler.pkl', 'rb') as f2:
 with open('./model/type_encoder.pkl', 'rb') as f3:
     type_encoder = pickle.load(f3)
 
-def prepare_input(type_battery, capacity, re, rct):
+def prepare_input(input):
     # encode
-    type_encoded = type_encoder.transform(np.array([[type_battery.lower()]])) # array(1, 3)
+    type_encoded = type_encoder.transform(np.array([[input['Type'].lower()]])) # # array(1, 3)
     
-    X_remain = np.array([[capacity, re, rct]]) # array(1, 3)
+    # scale
+    X_remain = np.array([[input['Capacity'], input['Re'], input['Rct']]]) # array(1, 3)
+    X_remain = X_scaler.transform(X_remain)
+
+    #concatenate
     X_predict = np.concatenate((type_encoded, X_remain), axis=1)  # array(1, 6)
 
-    # scale
-    X_predict = X_scaler.transform(X_predict)
-    
     return X_predict
 
-def predict_battery_life(**predict_input):
-    X_predict = prepare_input(**predict_input)
-
-    # ambient_temperature
+def predict_battery_life(input):
+    X_predict = prepare_input(input)
+    
     Y_predict = model.predict(X_predict)
 
     return Y_scaler.inverse_transform(Y_predict)
 
-Y_predict = predict_battery_life(type_battery = 'discharge', capacity = 1.674305, re = -4.976500e+11, rct = 1.055903e+12)
+input = {
+    "Type": 'discharge', 
+    "Capacity": 0.990759, 
+    "Re": 0.072553, 
+    "Rct": 0.101419
+}
 
-print(f"Predicted Battery Life: {Y_predict}")
+Y_predict = predict_battery_life(input)
+
+print(f"Predicted ambient_temperature: {Y_predict}")
